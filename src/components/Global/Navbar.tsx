5fe83e2bea9e90be111e41bc3ebc5f3aa8248de1
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-05-14 16:12:08 +0530

    New

--- src/components/Global/Navbar.tsx
+++ src/components/Global/Navbar.tsx
@@ -4,22 +4,18 @@ type Props = {};
 
 const Navbar = async (props: Props) => {
   return (
-    <header
-      className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40
-        backdrop-blur-lg-z-[100] flex items-center border-b-[1px]
-        border-neutral-900 justify-between"
-    >
-      <aside className="flex items-centre gap-[2px]">
-        <p className="text-white font-bold text-[18px]">Fu</p>
-        <image
-          src="/fuzzieLogo.png"
-          width={15}
-          height={15}
-          alt="fuzzie logo"
-          className="shadow-sm"
-        />
-        <p className="text-3xl font-bold">zie</p>
-      </aside>
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+    <aside className="flex items-center gap-[2px]">
+      <p className="text-3xl font-bold">Fu</p>
+      <image
+        src="/fuzzieLogo.png"
+        width={15}
+        height={15}
+        alt="fuzzie logo"
+        className="shadow-sm"
+      />
+      <p className="text-3xl font-bold">zie</p>
+    </aside>
     </header>
   );
 };
