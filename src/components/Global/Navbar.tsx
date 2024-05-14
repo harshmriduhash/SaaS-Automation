7755d953cce7b4c4c61f9d50be12e84aff8820b6
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-05-14 16:14:44 +0530

    New

--- src/components/Global/Navbar.tsx
+++ src/components/Global/Navbar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "lucide-react";
 import React from "react";
 
 type Props = {};
@@ -16,6 +17,28 @@ const Navbar = async (props: Props) => {
       />
       <p className="text-3xl font-bold">zie</p>
     </aside>
+    <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="#">Products</Link>
+          </li>
+          <li>
+            <Link href="#">Pricing</Link>
+          </li>
+          <li>
+            <Link href="#">Clients</Link>
+          </li>
+          <li>
+            <Link href="#">Resources</Link>
+          </li>
+          <li>
+            <Link href="#">Documentation</Link>
+          </li>
+          <li>
+            <Link href="#">Enterprise</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
