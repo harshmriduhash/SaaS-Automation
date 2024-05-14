6e7b805c60024342354f41af23ef175d8d33ede8
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-05-14 16:01:02 +0530

    New

--- src/components/Global/Navbar.tsx
+++ src/components/Global/Navbar.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 type Props = {}
 
-const Navbar = async {props: Props} => {
+const Navbar = async (props: Props) => {
     return (
         <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40
         backdrop-blur-lg-z-[100] flex items-center border-b-[1px]
-        border-neutral-900 justify-between"></header>
+        border-neutral-900 justify-between">
+            <aside className="flex items-centre gap-[2px]">
+                <p className="text-white font-bold text-[18px]">Auto-mate</p>
+            </aside>
+        </header>
     )
 }
 
